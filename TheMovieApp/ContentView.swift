7f0bee5b10d4da07f766b6729c195629b17026f0
Joabe Santana <joabesantana.c@gmail.com>
2023-11-08 21:15:52 -0300

    first commit

---
+++ TheMovieApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheMovieApp
+//
+//  Created by Joabe Santana Correia on 08/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheMovieApp/TheMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieAppApp.swift
+//  TheMovieApp
+//
+//  Created by Joabe Santana Correia on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
